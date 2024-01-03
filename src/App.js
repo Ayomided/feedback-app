8fa1186d8c1d4823ce38156c1b13020422278abe
Ayomided <adediji.d@yahoo.com>
2024-01-03 15:47:59 +0000

    feat: implement fade animation for feedback list

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
-import FeedbackForm from "./components/FeedbackForm";
+import FeedbackForm from "./components/FeedbackForm"
+import { v4 as uuidv4 } from "uuid";
 
 function App (){  
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')){
@@ -18,7 +24,7 @@ function App (){
     <>
     <Header />
      <div className = 'container'>
-      <FeedbackForm />
+      <FeedbackForm handleAdd = {addFeedback}/>
       <FeedbackStats feedback={feedback} />
       <FeedbackList feedback={feedback}
       handleDelete={deleteFeedback}
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,21 +1,44 @@
 import FeedbackItem from "./FeedbackItem"
 import PropTypes from 'prop-types'
+import {motion, AnimatePresence} from 'framer-motion'
 
 function FeedbackList({feedback, handleDelete}) {
     if(!feedback || feedback.length === 0) {
         return <p>No feedback yet</p>
     }
-  return (
+    // This returns the feedback list wih an animation
+    return (
     <div className="feedback-list">
-        {feedback.map((item) => (
+        <AnimatePresence>
+            {feedback.map((item) => (
+            <motion.div 
+            key={item.id}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            >
             <FeedbackItem 
             key={item.id} 
             item={item} 
             handleDelete={handleDelete}
             />
-        ))}
+            </motion.div>
+            ))}
+        </AnimatePresence>
     </div>
   )
+  // this does not contain any fade animation
+//   return (
+//     <div className="feedback-list">
+//         {feedback.map((item) => (
+//             <FeedbackItem 
+//             key={item.id} 
+//             item={item} 
+//             handleDelete={handleDelete}
+//             />
+//         ))}
+//     </div>
+//   )
 }
 
 FeedbackList.propTypes = {
