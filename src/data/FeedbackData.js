68715157e7f3c2ae14750dd59178f908915da242
Ayomided <adediji.d@yahoo.com>
2024-01-03 00:59:47 +0000

    feat: implement feedback cards and rating counter

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aliquam doloribus laudantium minus dolores voluptatum alias porro incidunt, eaque modi inventore tenetur consequatur dolorum impedit sed perspiciatis! Vero, eveniet ut',
+    },
+    {
+        id: 2,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aliquam doloribus laudantium minus dolores voluptatum alias porro incidunt, eaque modi inventore tenetur consequatur dolorum impedit sed perspiciatis! Vero, eveniet ut',
+    },
+    {
+        id: 3,
+        rating: 7,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aliquam doloribus laudantium minus dolores voluptatum alias porro incidunt, eaque modi inventore tenetur consequatur dolorum impedit sed perspiciatis! Vero, eveniet ut',
+    }
+]
+
+export default FeedbackData
